b7b337ff11c5386016f7ac9b5f37506c092eaaba
Alberto Oliveira <alberto.olv00@gmail.com>
2025-06-26 17:27:33 -0300

    Update ScreenSoundContext.cs

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -15,7 +15,9 @@ public class ScreenSoundContext: DbContext
     public DbSet<Musica> Musicas { get; set; }
     public DbSet<Genero> Generos { get; set; }
 
-    private string connectionString = "Data Source=Bebeto\\SQLSERVER;Initial Catalog=ScreenSoundV0; Integrated Security=True;Trust Server Certificate=True;Application Name=vscode-mssql;Application Intent=ReadWrite";
+    //private string connectionString = "Data Source=Bebeto\\SQLSERVER;Initial Catalog=ScreenSoundV0; Integrated Security=True;Trust Server Certificate=True;Application Name=vscode-mssql;Application Intent=ReadWrite";
+
+    private string connectionString = "Server=tcp:screensoundv0.database.windows.net,1433;Initial Catalog=ScreenSoundV0;Persist Security Info=False;User ID=alberto;Password=Bebeto123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
